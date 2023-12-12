efa9bf7ebec0d06a4fd8ab2ccd08130ea5fb547a
NavodyaTGCF <144510287+NavodyaTGCF@users.noreply.github.com>
2023-12-12 12:22:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function calculateTotalCost() {
   const underFive = parseInt(underFiveInput.value) || 0;
 
   
-  const roomCost = singleRooms * 25000 + doubleRooms * 35000 + tripleRooms * 45000 + extraRooms * 8000 +aboveFive*5000;
+  const roomCost = (singleRooms * 25000 + doubleRooms * 35000 + tripleRooms * 45000 + extraRooms * 8000 +aboveFive*5000)* adults
    
 
   let totalCost = roomCost;
